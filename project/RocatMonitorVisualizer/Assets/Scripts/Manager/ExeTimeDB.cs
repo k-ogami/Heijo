a4c8bcdc583d2dc226cccb3e25a16d568116a0c2
ogami <ogami>
2017-02-08 20:19:51 +0900

    * リポジトリ直下のディレクトリ構造変更 releaseを追加して、projectとreleaseのふたつ。 * Visualizerの更新 マウスカーソルを合わせてパネルが表示されるよう変更。

---
+++ project/RocatMonitorVisualizer/Assets/Scripts/Manager/ExeTimeDB.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class ExeTimeDB : MonoBehaviour
+{
+
+  public float MaxTime = 0;
+  public float HeightHistory = 0;
+  public float Height = 0;
+
+  private LinkedList<TimeData> timeDataList = new LinkedList<TimeData>();
+  private float history = 0;
+
+  public class TimeData
+  {
+    public long Time_ns;
+    public ExeTimeInfo[] ExeTimeInfos;
+  }
+
+  public void RegistInfo(ExeTimeInfo[] infos, long time)
+  {
+    // データを追加
+    TimeData timeData = new TimeData();
+    timeData.Time_ns = time;
+    timeData.ExeTimeInfos = infos;
+    timeDataList.AddLast(timeData);
+    // 最長保有時間を超えると古いデータから削除
+    while (timeDataList.Count != 0 && MaxTime * Mathf.Pow(10, 9) < timeDataList.Last.Value.Time_ns - timeDataList.First.Value.Time_ns) {
+      timeDataList.RemoveFirst();
+    }
+  }
+
+  public void SetHeight()
+  {
+    // <メソッドID, <スレッドID, 実行時間>>
+    Dictionary<long, Dictionary<long, long>> dic = new Dictionary<long, Dictionary<long, long>>();
+
+    // 時間範囲内のメソッド毎スレッド毎の実行時間を取得
+    long last = timeDataList.Last.Value.Time_ns;
+    long length = (long)(HeightHistory * Mathf.Pow(10, 9));
+    for (LinkedListNode<TimeData> i = timeDataList.Last; i != null && last - i.Value.Time_ns < length; i = i.Previous) {
+      foreach (ExeTimeInfo info in i.Value.ExeTimeInfos) {
+        if (!dic.ContainsKey(info.MethodID)) {
+          dic[info.MethodID] = new Dictionary<long, long>();
+          dic[info.MethodID][info.ThreadID] = info.ExeTime;
+        }
+        else {
+          if (!dic[info.MethodID].ContainsKey(info.ThreadID)) {
+            dic[info.MethodID].Add(info.ThreadID, info.ExeTime);
+          }
+          else {
+            dic[info.MethodID][info.ThreadID] += info.ExeTime;
+          }
+        }
+      }
+      // 保有時間の更新
+      history = last - i.Value.Time_ns;
+    }
+    // オブジェクトの高さを設定
+    RecSetHeight(Manager.CityObjectDB.DefaultPackage, dic);
+  }
+
+  private void RecSetHeight(CityObject obj, Dictionary<long, Dictionary<long, long>> dic)
+  {
+    obj.Height = GetHeight(obj, dic);
+    foreach (CityObject child in obj.PackageChildren.Values) {
+      RecSetHeight(child, dic);
+    }
+    foreach (CityObject child in obj.ClassChildren.Values) {
+      RecSetHeight(child, dic);
+    }
+    foreach (CityObject child in obj.MethodChildren.Values) {
+      RecSetHeight(child, dic);
+    }
+  }
+
+  private float GetHeight(CityObject obj, Dictionary<long, Dictionary<long, long>> dic)
+  {
+    obj.Time = GetTime(obj, dic);
+    float height;
+    height = history != 0 ? obj.Time / history * Height : 0;
+    return height;
+  }
+
+  private long GetTime(CityObject obj, Dictionary<long, Dictionary<long, long>> dic)
+  {
+    // オブジェクト以下にあるメソッドID（あるいは自身のメソッドID）を再帰的に取得
+    LinkedList<long> methods = RecGetUnderMethodID(obj);
+    // スレッド毎時間に変換
+    Dictionary<long, long> dic2 = new Dictionary<long, long>();
+    foreach (long method in methods) {
+      if (dic.ContainsKey(method)) {
+        foreach (long thread in dic[method].Keys) {
+          if (!dic2.ContainsKey(thread)) {
+            if (dic[method].ContainsKey(thread)) {
+              dic2[thread] = dic[method][thread];
+            }
+          }
+          else {
+            if (dic[method].ContainsKey(thread)) {
+              dic2[thread] += dic[method][thread];
+            }
+          }
+        }
+      }
+    }
+    // 各スレッドで最も長い実行時間から高さを算出
+    long time = 0;
+    foreach (long t in dic2.Values) {
+      if (time < t) {
+        time = t;
+      }
+    }
+    // スレッド数もここで更新
+    obj.ThreadNum = dic2.Count;
+
+    return time;
+  }
+
+  private LinkedList<long> RecGetUnderMethodID(CityObject obj, LinkedList<long> list = null)
+  {
+    if (list == null) {
+      list = new LinkedList<long>();
+    }
+    if (obj.IsMethod) {
+      list.AddLast(((MethodObject)obj).MethodID);
+    }
+    else {
+      foreach (CityObject child in obj.PackageChildren.Values) {
+        RecGetUnderMethodID(child, list);
+      }
+      foreach (CityObject child in obj.ClassChildren.Values) {
+        RecGetUnderMethodID(child, list);
+      }
+      foreach (CityObject child in obj.MethodChildren.Values) {
+        RecGetUnderMethodID(child, list);
+      }
+    }
+    return list;
+  }
+
+}
---
+++ project/RocatMonitorVisualizer/Assets/Scripts/UI/VisibleChanger.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System.Text;
+
+public class VisibleChanger : MonoBehaviour
+{
+
+  public Color CursoredColor = Color.black;
+  public float DescriptTime = 0;
+
+  [System.NonSerialized]
+  public CityObject CursoredObject = null;
+
+  private Color originalColor = Color.black;
+  private float time = 0;
+
+  private void Update()
+  {
+    CheckCursor();
+    ChangeByClick();
+  }
+
+  private void CheckCursor()
+  {
+    // レイを飛ばしてオブジェクトを取得
+    CityObject cursored = null;
+    if (0 <= Input.mousePosition.x
+      && Input.mousePosition.x <= Screen.width
+      && 0 <= Input.mousePosition.y
+      && Input.mousePosition.y <= Screen.height) {
+      Ray ray = UI.CameraController.Camera.ScreenPointToRay(Input.mousePosition);
+      RaycastHit hit;
+      if (Physics.Raycast(ray, out hit, float.MaxValue, 1 << LayerMask.NameToLayer("VisibleCityObject"))) {
+        cursored = hit.collider.gameObject.GetComponent<CityObject>();
+     }
+    }
+    // 選択中のオブジェクトが変化した場合
+    if (CursoredObject != cursored) {
+      time = 0;
+      UI.Descriptor.SetVisible(false);
+      // 選択から外れたオブジェクトの色を戻す
+      if (CursoredObject != null) {
+        CursoredObject.GetComponent<Renderer>().material.color = originalColor;
+        CursoredObject = null;
+      }
+      // 新たに選択されたオブジェクトの色を変更する
+      if (cursored != null) {
+        CursoredObject = cursored.GetComponent<CityObject>();
+        originalColor = cursored.GetComponent<Renderer>().material.color;
+        cursored.GetComponent<Renderer>().material.color = CursoredColor;
+      }
+    }
+    else {
+      if (CursoredObject != null) {
+        SetText();
+        // 一定時間カーソルを合わせていたら、説明用のパネルを表示
+        if (DescriptTime < time) {
+          UI.Descriptor.SetVisible(true);
+          UI.Descriptor.SetTransform(Input.mousePosition);
+        }
+        else {
+          time += Time.deltaTime;
+        }
+      }
+    }
+  }
+
+  private void ChangeByClick()
+  {
+    bool click = Input.GetMouseButtonDown(0);
+    if (click && CursoredObject != null && !CursoredObject.IsMethod) {
+      if (CursoredObject.ChildrenVisivle) {
+        CursoredObject.ChildrenVisivle = false;
+        RecSetChildrenVisible(CursoredObject, false);
+      }
+      else {
+        CursoredObject.ChildrenVisivle = true;
+        RecSetChildrenVisible(CursoredObject, true);
+      }
+    }
+  }
+
+  private void RecSetChildrenVisible(CityObject obj, bool visible)
+  {
+    foreach (CityObject child in obj.PackageChildren.Values) {
+      child.Visible = visible;
+      if (child.ChildrenVisivle) {
+        RecSetChildrenVisible(child, visible);
+      }
+    }
+    foreach (CityObject child in obj.ClassChildren.Values) {
+      child.Visible = visible;
+      if (child.ChildrenVisivle) {
+        RecSetChildrenVisible(child, visible);
+      }
+    }
+    foreach (CityObject child in obj.MethodChildren.Values) {
+      child.Visible = visible;
+      if (child.ChildrenVisivle) {
+        RecSetChildrenVisible(child, visible);
+      }
+    }
+  }
+
+  private void SetText()
+  {
+    StringBuilder str = new StringBuilder();
+    if (CursoredObject.IsPackage) {
+      str.Append("Package Name\t:" + CursoredObject.name);
+    }
+    else if (CursoredObject.IsClass) {
+      str.Append("Class Name\t:" + CursoredObject.name);
+    }
+    else  {
+      str.Append("Method Name\t:" + ((MethodObject)CursoredObject).GetPerfectName());
+    }
+    float rate = CursoredObject.Time / (Manager.ExeTimeDB.HeightHistory * Mathf.Pow(10, 9));
+    if (1 < rate) rate = 1;
+    if (0.01 < rate || rate == 0) {
+      str.Append("\nExecution Rate\t:" + rate.ToString("0.00%"));
+    }
+    else {
+      str.Append("\nExecution Rate\t:" + (rate * 100).ToString("E2") + "%");
+    }
+    str.Append("\nThread Num\t\t:" + CursoredObject.ThreadNum);
+    UI.Descriptor.SetText(str.ToString());
+  }
+
+}
