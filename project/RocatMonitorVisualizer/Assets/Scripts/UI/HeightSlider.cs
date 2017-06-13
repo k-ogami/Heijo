58ba033c1be0f61597df5b2a3ada1c047b04c186
k-ogami <ogami.katsuya.ny7@is.naist.jp>
2017-06-13 15:29:46 +0900

    HeightSlider変更

---
+++ project/RocatMonitorVisualizer/Assets/Scripts/UI/HeightSlider.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HeightSlider : MonoBehaviour
+{
+
+  [System.NonSerialized]
+  public Slider Slider = null;
+
+  protected void Awake()
+  {
+    Slider = GetComponent<Slider>();
+
+    // イベントの追加
+    Slider.onValueChanged.AddListener(OnValueChanged);
+  }
+
+  private void OnValueChanged(float value)
+  {
+    // 高さを更新
+    RecSetHeight(Manager.CityObjectDB.DefaultPackage);
+  }
+
+  private void RecSetHeight(CityObject obj)
+  {
+    obj.SetHeight(obj.Height_0_1);
+    foreach (CityObject child in obj.PackageChildren.Values) {
+      RecSetHeight(child);
+    }
+    foreach (CityObject child in obj.ClassChildren.Values) {
+      RecSetHeight(child);
+    }
+    foreach (CityObject child in obj.MethodChildren.Values) {
+      RecSetHeight(child);
+    }
+  }
+
+}
