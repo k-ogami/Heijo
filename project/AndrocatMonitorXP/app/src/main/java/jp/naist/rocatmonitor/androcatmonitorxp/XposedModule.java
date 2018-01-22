b8ab3295e4d0fb432ca669c536cca227382177ab
k-ogami <ogami.katsuya.ny7@is.naist.jp>
2018-01-22 15:47:50 +0900

    mac用ビジュアライザの追加

--- project/AndrocatMonitorXP/app/src/main/java/jp/naist/rocatmonitor/androcatmonitorxp/XposedModule.java
+++ project/AndrocatMonitorXP/app/src/main/java/jp/naist/rocatmonitor/androcatmonitorxp/XposedModule.java
@@ -20,8 +20,9 @@ public class XposedModule implements IXposedHookLoadPackage
   {
     // FIXME Noxのときでは使えた省略処理が実機（Android7.0,huawei nova lite）では動作しない？
     //if (!loadPackageParam.isFirstApplication || !loadPackageParam.packageName.equals(loadPackageParam.processName)) return;
-    // FIXME GoogleStoreでインストール時にBadParcelableException: ClassNotFoundException when unmarchalling。Noxだけ？
-    //if (loadPackageParam.packageName.equals("com.android.vending")) return;
+
+    // FIXME GoogleStoreでインストール時にBadParcelableException: ClassNotFoundException when unmarchalling
+    if (loadPackageParam.packageName.equals("com.android.vending")) return;
 
     // アプリケーションの起動にフックする
     XposedHelpers.findAndHookMethod(
